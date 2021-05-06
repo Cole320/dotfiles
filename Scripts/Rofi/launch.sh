3d550098dc04ff733c867ac2d28cb175946dd528
Cole320 <colebrazinski15@gmail.com>
2021-05-06 18:05:41 -0400

    Refactored structure and added flameshot support

--- Scripts/Rofi/launch.sh
+++ Scripts/Rofi/launch.sh
@@ -3,12 +3,12 @@
 menu="$1"
 
 if [ "$menu" = "music" ]; then
-    rofi -modi 'Music:~/scripts/rofi/music.sh' -show Music -theme music-launcher
+    rofi -modi 'Music:~/Scripts/Rofi/music.sh' -show Music -theme music-launcher
 elif [ "$menu" = "appmenu" ]; then
     rofi -show drun -theme clean
 elif [ "$menu" = "powermenu" ]; then
-#    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu
-    rofi -modi 'Powermenu:~/scripts/rofi/powermenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -24 -yoffset 70
+#    rofi -modi 'Powermenu:~/Scripts/Rofi/powerMenu.sh' -show Powermenu -theme powermenu
+    rofi -modi 'Powermenu:~/Scripts/Rofi/powerMenu.sh' -show Powermenu -theme powermenu -location 3 -xoffset -24 -yoffset 70
 elif [ "$menu" = "tabmenu" ]; then
-	sh ~/Scripts/Rofi/tabmenu.sh
+	sh ~/Scripts/Rofi/tabMenu.sh
 fi
